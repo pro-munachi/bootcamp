146b3296bedc27cc32aca5c96328909b8b0f4914
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2022-04-18 19:01:14 +0100

    created class model

---
+++ models/classModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const moment = require('moment')
+
+const classSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+
+    type: {
+      type: String,
+      required: true,
+    },
+
+    dispatcherId: {
+      type: String,
+    },
+
+    students: [
+      {
+        name: { type: mongoose.Schema.Types.ObjectId, required: true },
+      },
+    ],
+
+    date: {
+      type: String,
+      required: true,
+      default: moment(new Date()).format('YYYYMMDD'),
+    },
+
+    isDeleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+)
+
+const Class = mongoose.model('Class', classSchema)
+
+module.exports = Class
